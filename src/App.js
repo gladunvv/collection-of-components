c561b892cd82c84fd467f859a8adaa8f907d2855
vladislav <bincha.1997@gmail.com>
2020-06-26 23:13:32 +0900

    feat: update build

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { hot } from 'react-hot-loader/root';
+
 import React, { Component } from 'react';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1> Hello, World! </h1>
+        <h1> Hello, World!</h1>
       </div>
     );
   }
 }
 
-export default App;
+export default hot(App);
