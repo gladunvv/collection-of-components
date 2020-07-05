43a34776216fcfbcc0841ce9ec8e8b909d1ca10b
vladislav <bincha.1997@gmail.com>
2020-07-05 23:59:02 +0900

    feat: update component

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import cn from 'classnames';
 
 import './BurgerMenu.scss';
 
 export class BurgerMenu extends Component {
+  state = {
+    burgerOpen: false,
+  };
+  toggleBurger = () => {
+    const position = this.state.burgerOpen;
+    this.setState({ burgerOpen: !position });
+  };
+
   render() {
-    return <>BurgerMenu</>;
+    const burgerClass = cn('burger', { on: this.state.burgerOpen });
+    return (
+      <div className='wrapper-burger'>
+        <a href='#' className={burgerClass} onClick={this.toggleBurger}>
+          <div className='bar bun-top'></div>
+          <div className='bar lettuce'></div>
+          <div className='bar mustard'></div>
+          <div className='bar ketchup'></div>
+          <div className='bar patty'></div>
+          <div className='bar bun-bot'></div>
+        </a>
+      </div>
+    );
   }
 }
